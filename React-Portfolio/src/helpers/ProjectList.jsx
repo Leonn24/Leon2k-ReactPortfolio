d933fe25828366737875a3fc539ffbc9f18a9c35
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2024-03-26 14:10:50 -0400

    added images created projectList for displaying projects and added HTML for projects page

---
+++ React-Portfolio/src/helpers/ProjectList.jsx
@@ -0,0 +1,27 @@
+import Proj1 from '../assets/images/Valorantblog.png';
+import Proj2 from '../assets/images/eventslist.png';
+import Proj3 from '../assets/images/Startquiz.png';
+import Proj4 from '../assets/images/weathersearchedcity.png';
+
+export const ProjectList = [
+    {
+        name: 'Valorant Blog',
+        image: Proj1,
+        skills: "JavaScript,HTML,CSS,MySql,Handlebars"
+    },
+    {
+        name: 'Event Geek',
+        image: Proj2,
+        skills: "JavaScript,HTML,CSS"
+    },
+    {
+        name: 'Basic Web Dev Quiz',
+        image: Proj3,
+        skills: "JavaScript,HTML,CSS"
+    },
+    {
+        name: 'Weather App',
+        image: Proj4,
+        skills: "JavaScript,HTML,CSS"
+    }
+];
--- React-Portfolio/src/pages/Projects.jsx
+++ React-Portfolio/src/pages/Projects.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ProjectItem from '../components/ProjectItem';
-// import Img
+import { ProjectList } from '../helpers/ProjectList';
+import '../styles/Projects.css'
+
+
 
 function Projects() {
     return (
     <div className='projects'>
         <h1>My Projects</h1>
         <div className='projectList'>
-            <ProjectItem />
-            <ProjectItem />
+           {ProjectList.map((project) => {
+            return <ProjectItem name={project.name} image={project.image} />;
+           })}
         </div>
     </div>
     ) 
