c7f0258bc429664bff8f3e2c904831389c803318
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2024-03-26 23:28:10 -0400

    added routes to projects individually and linked the github repo urls, created projectDisplay page

--- React-Portfolio/src/helpers/ProjectList.jsx
+++ React-Portfolio/src/helpers/ProjectList.jsx
@@ -7,21 +7,25 @@ export const ProjectList = [
     {
         name: 'Valorant Blog',
         image: Proj1,
-        skills: "JavaScript,HTML,CSS,MySql,Handlebars"
+        skills: "JavaScript,HTML,CSS,MySql,Handlebars",
+        githubUrl: "https://github.com/Leonn24/Techblog.exe"
     },
     {
         name: 'Event Geek',
         image: Proj2,
-        skills: "JavaScript,HTML,CSS"
+        skills: "JavaScript,HTML,CSS",
+        githubUrl: 'https://github.com/Leonn24/EventGeek-Project1'
     },
     {
         name: 'Basic Web Dev Quiz',
         image: Proj3,
-        skills: "JavaScript,HTML,CSS"
+        skills: "JavaScript,HTML,CSS",
+        githubUrl: 'https://github.com/Leonn24/quiz-week4'
     },
     {
         name: 'Weather App',
         image: Proj4,
-        skills: "JavaScript,HTML,CSS"
+        skills: "JavaScript,HTML,CSS",
+        githubUrl: 'https://github.com/Leonn24/5day-weather'
     }
 ];
--- React-Portfolio/src/pages/Home.jsx
+++ React-Portfolio/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
+import '../styles/Home.css'
+
 
 function Home() {
   return (
